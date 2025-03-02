76e8a2bd45009847a017a330026d9bd39b37082b
илья <Oksimonov@mail.ru>
2025-03-02 23:24:01 +0300

    Окончательный результат

--- lab_1/Task_2/main.py
+++ lab_1/Task_2/main.py
@@ -13,6 +13,7 @@ task_text = ("КwЧ5Д>ЫХЧ1ЪЕt Й2>ХИЬЧЙ ФХ 1 ХБЧБХЫПЫХЪ"
              " ХtЫЧ1 ЯЕЩЧД>ХБЧБХЕЙwЫ17Д>ЫХЧ1ЪЕt Й2>Х Х" 
              " ИrЕ17ЯКУЙИФХtЧЯwЫ17ДЕ")
 
+
 rus_freq = {
     'о': 0.0965, 'и': 0.0753, 'е': 0.0723, 'а': 0.0648, 'н': 0.0618,
     'т': 0.0616, 'с': 0.0520, 'р': 0.0407, 'в': 0.0393, 'м': 0.0298,
@@ -25,6 +26,17 @@ rus_freq = {
     'э': 0.0024, 'ъ': 0.0004, 'ё': 0.0004, ' ': 0.1287
 }
 
+
+def write_to_file(filename, content):
+    """
+    Записывает содержимое в файл.
+    :param filename: имя файла
+    :param content: содержимое для записи
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(content)
+
+
 def replace_chars(text, target_char, replacement_char):
     """
     Заменяет все вхождения target_char на replacement_char в тексте.
@@ -34,6 +46,7 @@ def replace_chars(text, target_char, replacement_char):
     :return: текст с выполненными заменами
     """
     return text.replace(target_char, replacement_char)
+
 
 def calculate_char_percentages(text):
     """
@@ -59,7 +72,6 @@ def calculate_char_percentages(text):
     return char_percentages
 
 
-
 def main():
     text = task_text
 
@@ -81,16 +93,12 @@ def main():
             text = text.replace("2", "З")
         elif text[i] == "1":
             text = text.replace("1", "Н")
-        elif text[i] == "0":
-            text = text.replace("0", "Р")
         elif text[i] == "w":
             text = text.replace("w", "С")
         elif text[i] == "r":
             text = text.replace("r", "Т")
         elif text[i] == "t":
             text = text.replace("t", "Ц")
-        elif text[i] == "y":
-            text = text.replace("y", "Ш")
         elif text[i] == ">":
             text = text.replace(">", "Э")
     print("______________________________________________________________\n\nСвод к единому алфавиту:\n")
@@ -106,9 +114,7 @@ def main():
         sorted_dict[key] = percent_dict[key]
     print(sorted_dict)
 
-
     print("\n______________________________________________________________\n\nДешифрованный текст:\n")
-
     text = replace_chars(text, " ", "и") #
     text = replace_chars(text, "Е", "о") #
     text = replace_chars(text, "Д", "н") #
@@ -137,7 +143,7 @@ def main():
     text = replace_chars(text, "Ь", "ж")
     text = replace_chars(text, "Л", "ф")
     text = replace_chars(text, "М", "х")
-    text = replace_chars(text, "У", "ю")
+    #text = replace_chars(text, "y", "ю")
 
     text = replace_chars(text, "Ю", "ц")
     text = replace_chars(text, "О", "ш")
@@ -150,8 +156,47 @@ def main():
     #Замена в конце дабы исключить повторения символа " " в алфавите
     text = replace_chars(text, "Х", " ")
 
+    crypt_key = {
+        "r": "п",
+        "w": "с",
+        "t": "р",
+
+        "1": "л",
+        "2": "м",
+        "4": "ц",
+        "5": "ч",
+        "7": "ь",
+        "8": "э",
+
+        "А": "й",
+        "Б": "к",
+        "Д": "н",
+        "Е": "о",
+        "И": "с",
+        "Й": "т",
+        "К": "у",
+        "Л": "ф",
+        "М": "х",
+        "О": "ш",
+        "П": "щ",
+        "У": "ю",
+        "Ф": "я",
+        "Х": " ",
+        "Ч": "а",
+        "Щ": "в",
+        "Ъ": "г",
+        "Ы": "е",
+        "Ь": "ж",
+        "Я": "з",
+    }
+
     print(text)
     print("\n")
+
+    #Запись в файлы
+    write_to_file("encrypted_text.txt", task_text)
+    write_to_file("decrypted_text.txt", text)
+    write_to_file("decrypt_key.txt", str(crypt_key))
 
 if __name__ == "__main__":
     main()
