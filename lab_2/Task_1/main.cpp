52529ac224d7e7251001df2325b420428ea5490d
илья <Oksimonov@mail.ru>
2025-03-05 22:38:17 +0300

    Добавление последнего теста на самую длинную последовательность единиц в блоке + проверка тестов (последний тест требует использования калькулятора гамма функций)

--- lab_2/Task_1/main.cpp
+++ lab_2/Task_1/main.cpp
@@ -7,7 +7,7 @@ int main() {
     //Запускать с GDB online debugger
     // Инициализация генератора случайных чисел. Зависит от времени с 1970 года
     std::srand(std::time(0));
-    std::ofstream outfile("random_seq_cpp.txt");
+    std::ofstream outfile("Random_seq_cpp.txt");
 
     for (int i = 0; i < 128; ++i) {
         int bit = std::rand() % 2;
@@ -15,6 +15,6 @@ int main() {
     }
 
     outfile.close();
-    std::cout << "save to random_seq_cpp.txt" << std::endl;
+    std::cout << "save to Random_seq_cpp.txt" << std::endl;
     return 0;
 }
--- lab_2/Task_1/main.java
+++ lab_2/Task_1/main.java
@@ -5,12 +5,12 @@ import java.util.Random;
 class RandomGenerator {
     public static void main(String[] args) {
         Random rand = new Random();
-        try (FileWriter writer = new FileWriter("random_seq_java.txt")) {
+        try (FileWriter writer = new FileWriter("Random_seq_java.txt")) {
             for (int i = 0; i < 128; i++) {
                 int bit = rand.nextInt(2); //nextInt(2) - от 0 до 2
                 writer.write(Integer.toString(bit));
             }
-            System.out.println("save to random_seq_java.txt");
+            System.out.println("save to Random_seq_java.txt");
         } catch (IOException e) {
             e.printStackTrace();
         }
